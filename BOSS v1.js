381d582d2e4603bdc2e6ed9ed064554f8586a57e
Julietta <hypatia1186@gmail.com>
2018-03-25 14:39:12 -0500

    fixed for IE

--- BOSS v1.js
+++ BOSS v1.js
@@ -11,12 +11,12 @@ function openCity(evt, cityName) {
     // Get all elements with class="tablinks" and remove the class "active"
      tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active1", "");
     }
 
     // Show the current tab, and add an "active" class to the link that opened the tab
      document.getElementById(cityName).style.display = "block";
-     evt.currentTarget.className += " active";
+     evt.currentTarget.className += " active1";
 
 	 } 
 
@@ -50,9 +50,5 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-<<<<<<< HEAD
-  timer = setTimeout(showSlides, 7000);
-=======
   timer = setTimeout(showSlides, 4000);
->>>>>>> master
 } 
